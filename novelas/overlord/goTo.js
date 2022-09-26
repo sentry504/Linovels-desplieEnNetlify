4a37c8251b1878dc39ab38f5b4ae1ca80267edce
sentry504 <59220885+sentry504@users.noreply.github.com>
2022-09-25 18:59:12 -0600

    deleted:    copia.html 	modified:   index.html 	deleted:    novelas/overlord/capitulo/previo.html 	deleted:    novelas/overlord/capitulo/siguiente.html 	new file:   novelas/overlord/capitulo/volumenI/overlord-vol1-capitulo1_parte1.html 	modified:   novelas/overlord/capitulo/volumenI/overlord-vol1-prologo.html 	modified:   novelas/overlord/capitulos.json 	modified:   novelas/overlord/goTo.js 	new file:   novelas/overlord/img/Overlord-Vol1-1-675x1024.jpg 	modified:   novelas/overlord/info.html 	modified:   source/css/listado.css 	modified:   source/css/sitio.css 	modified:   source/css/visor.css 	deleted:    source/fondo1.mp4 	deleted:    source/fondo2.mp4 	deleted:    source/fondo3.mp4

--- novelas/overlord/goTo.js
+++ novelas/overlord/goTo.js
@@ -2,7 +2,7 @@ function previo(){
     console.log("Presionaste Previo");
     let ir= 0;
     async function populate() {
-        const requestURL = "capitulos.json";
+        const requestURL = "../../capitulos.json";
         const request = new Request(requestURL);
     
         const response = await fetch(request);
@@ -13,12 +13,12 @@ function previo(){
     }   
 
     function populateData(obj){
-        const Iframe = document.querySelector('iframe');
-        const identificador= Iframe.getAttribute('src');
+        const identificador= window.location.pathname
+        console.log("dir: ",identificador)
         const capitulos = obj.capitulos;
         
         for (const ID of capitulos){
-            if(`capitulo/${ID.capitulo}`==identificador){
+            if(ID.capitulo==identificador){
                 console.log("Valor de IR previo a la suma",ir);
                 ir= parseInt(ID.id)-1;
             }
@@ -26,18 +26,19 @@ function previo(){
         console.log("valor de IR con valor sumado: ",ir);
         for (const busqueda of capitulos){
             if(busqueda.id==ir){
-                Iframe.setAttribute('src',`capitulo/${busqueda.capitulo}`);
+                window.open(busqueda.capitulo,`_self`)
             }
         }
     }
     
     populate();
 }
+
 function siguiente(){
     console.log("Presionaste Siguiente");
     let ir= 0;
     async function populate() {
-        const requestURL = "capitulos.json";
+        const requestURL = "../../capitulos.json";
         const request = new Request(requestURL);
     
         const response = await fetch(request);
@@ -48,12 +49,12 @@ function siguiente(){
     }   
 
     function populateData(obj){
-        const Iframe = document.querySelector('iframe');
-        const identificador= Iframe.getAttribute('src');
+        const identificador= window.location.pathname
+        console.log("dir: ",identificador)
         const capitulos = obj.capitulos;
         
         for (const ID of capitulos){
-            if(`capitulo/${ID.capitulo}`==identificador){
+            if(ID.capitulo==identificador){
                 console.log("Valor de IR previo a la suma",ir);
                 ir= parseInt(ID.id)+1;
             }
@@ -61,7 +62,7 @@ function siguiente(){
         console.log("valor de IR con valor sumado: ",ir);
         for (const busqueda of capitulos){
             if(busqueda.id==ir){
-                Iframe.setAttribute('src',`capitulo/${busqueda.capitulo}`);
+                window.open(busqueda.capitulo,`_self`)
             }
         }
     }
